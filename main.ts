bed2fe688ce0da0368b401b0fd3259d0c0dcdfd3
ProfeMauro8721 <92501098+ProfeMauro8721@users.noreply.github.com>
2023-07-19 09:41:54 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,47 @@
 input.onButtonPressed(Button.A, function () {
-    basic.pause(1000)
-    servos.P1.setAngle(135)
+    basic.showIcon(IconNames.Sad)
     basic.pause(500)
-    servos.P1.setAngle(45)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.InBackground)
+    for (let index = 0; index < 3; index++) {
+        basic.showIcon(IconNames.Sad)
+        servos.P1.setAngle(45)
+        basic.pause(500)
+        servos.P1.setAngle(135)
+        basic.pause(500)
+    }
+    servos.P1.setAngle(90)
     basic.pause(500)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    basic.pause(500)
-    basic.showString("Hola")
     basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Nyan), music.PlaybackMode.InBackground)
+    for (let index = 0; index < 4; index++) {
+        servos.P1.setAngle(110)
+        basic.pause(200)
+        basic.showIcon(IconNames.Heart)
+        servos.P1.setAngle(70)
+        basic.pause(200)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+    basic.showIcon(IconNames.Happy)
+    servos.P1.setAngle(90)
+    basic.pause(500)
 })
-servos.P1.setAngle(45)
-for (let index = 0; index < 3; index++) {
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.pause(100)
+servos.P1.setAngle(90)
+for (let index = 0; index < 2; index++) {
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Half)), music.PlaybackMode.InBackground)
     basic.showIcon(IconNames.SmallSquare)
-    basic.pause(100)
-    basic.showIcon(IconNames.Target)
+    basic.pause(25)
+    basic.showIcon(IconNames.Square)
+    basic.pause(25)
 }
-basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+})
